a0e6c6977eaeafdf534b5d73f15061faef8d5d61
ditas <ditasandditas@gmail.com>
2022-05-27 21:25:17 +0100

    Remove lager, minor fixes

--- src/ditax_metric.erl
+++ src/ditax_metric.erl
@@ -1,11 +1,3 @@
-%%%-------------------------------------------------------------------
-%%% @author ditas
-%%% @copyright (C) 2019, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 22. Nov 2019 14:09
-%%%-------------------------------------------------------------------
 -module(ditax_metric).
 -author("ditas").
 
@@ -17,12 +9,14 @@
 -export([start_link/1]).
 
 %% gen_server callbacks
--export([init/1,
+-export([
+    init/1,
     handle_call/3,
     handle_cast/2,
     handle_info/2,
     terminate/2,
-    code_change/3]).
+    code_change/3
+]).
 
 -define(SERVER, ?MODULE).
 
@@ -36,12 +30,6 @@
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
 start_link(Metric) when is_record(Metric, metric) ->
     gen_server:start_link({local, Metric#metric.name}, ?MODULE, Metric, []);
 start_link(MetricName) ->
@@ -51,61 +39,39 @@ start_link(MetricName) ->
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
--spec(init(Args :: term()) ->
-    {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term()} | ignore).
+-spec init(Args :: atom() | #metric{}) ->
+    {ok, State :: #state{}}.
 init(Metric) when is_record(Metric, metric) ->
-    
-    io:format("--------INIT 1 ~p ~p~n", [?MODULE, Metric]),
-    
-    ets:new(Metric#metric.name, [ordered_set, named_table, {keypos, #metric_data.time}]),
-    State = case Metric#metric.period of
+    _Name = ets:new(Metric#metric.name, [ordered_set, named_table, {keypos, #metric_data.time}]),
+    State =
+        case Metric#metric.period of
+            undefined ->
+                #state{name = Metric#metric.name};
+            Period ->
+                {ok, TRef} = timer:send_interval(
+                    Period * 1000, {reset, Metric#metric.name, Period}
+                ),
+                #state{name = Metric#metric.name, period = Period, timer_ref = TRef}
+        end,
+    {ok, _} = case Metric#metric.show_every of
         undefined ->
-            #state{name = Metric#metric.name};
-        Period ->
-            {ok, TRef} = timer:send_interval(Period * 1000, {reset, Metric#metric.name, Period}),
-            #state{name = Metric#metric.name, period = Period, timer_ref = TRef}
-    end,
-    case Metric#metric.show_every of
-        undefined ->
-            ignore;
+            {ok, ignore};
         Interval ->
-            {ok, _} = timer:send_interval(Interval * 1000, show)
+            timer:send_interval(Interval * 1000, show)
     end,
     {ok, State};
 init(Metric) ->
-    
-    io:format("--------INIT 2 ~p ~p ~p~n", [is_record(Metric, metric), ?MODULE, Metric]),
-    
-    ets:new(Metric, [ordered_set, named_table, {keypos, #metric_data.time}]),
+    % io:format("--------INIT 2 ~p ~p ~p~n", [is_record(Metric, metric), ?MODULE, Metric]),
+
+    _Name = ets:new(Metric, [ordered_set, named_table, {keypos, #metric_data.time}]),
     {ok, #state{name = Metric}}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-    State :: #state{}) ->
-    {reply, Reply :: term(), NewState :: #state{}} |
-    {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-    {stop, Reason :: term(), NewState :: #state{}}).
+-spec handle_call(
+    Request :: term(),
+    From :: {pid(), Tag :: term()},
+    State :: #state{}
+) ->
+    {reply, Reply :: term(), NewState :: #state{}}.
 handle_call(get, _From, #state{name = MetricName, period = Period} = State) ->
     Count = count(MetricName, Period),
     {reply, Count, State};
@@ -115,89 +81,46 @@ handle_call({get, Period}, _From, #state{name = MetricName} = State) ->
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_cast(Request :: term(), State :: #state{}) ->
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), NewState :: #state{}}).
+-spec handle_cast(Request :: term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}}.
 handle_cast({increment, Value}, State) ->
     Key = erlang:system_time(second),
-    case ets:lookup(State#state.name, Key) of
+    _ = case ets:lookup(State#state.name, Key) of
         [] ->
             ets:insert(State#state.name, #metric_data{time = Key, value = Value});
         _ ->
-            _Result = ets:update_counter(State#state.name, Key, {#metric_data.value, Value})
+            ets:update_counter(State#state.name, Key, {#metric_data.value, Value})
     end,
     {noreply, State};
 handle_cast(_Request, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), NewState :: #state{}}).
+-spec handle_info(Info :: timeout() | term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}}.
 handle_info(show, State) ->
-    Count = count(State#state.name, State#state.period),
-    
-    io:format("~n~n-----------COUNT ~p----------- ~p~n", [State#state.name, Count]),
-    
+    _Count = count(State#state.name, State#state.period),
     {noreply, State};
 handle_info({reset, MetricName, Period}, State) ->
     StartTime = erlang:system_time(second) - Period,
-    MS = [{{'_', '$1','$2'},
-        [{'<','$1',StartTime}],
-        [true]}],
-    
-    %% TODO: проверить производительность
+    MS = [{{'_', '$1', '$2'}, [{'<', '$1', StartTime}], [true]}],
     _Result = ets:select_delete(MetricName, MS),
-    
     {noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-    State :: #state{}) -> term()).
+-spec terminate(
+    Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #state{}
+) -> term().
 terminate(_Reason, _State) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-    Extra :: term()) ->
-    {ok, NewState :: #state{}} | {error, Reason :: term()}).
+-spec code_change(
+    OldVsn :: term() | {down, term()},
+    State :: #state{},
+    Extra :: term()
+) ->
+    {ok, NewState :: #state{}}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
@@ -209,9 +132,7 @@ count(MetricName, Period) ->
     case Period =/= undefined of
         true ->
             StartTime = erlang:system_time(second) - Period,
-            MS = [{#metric_data{time = '$1',value = '$2'},
-                [{'>=','$1',StartTime}],
-                ['$_']}],
+            MS = [{#metric_data{time = '$1', value = '$2'}, [{'>=', '$1', StartTime}], ['$_']}],
             Selection = ets:select(MetricName, MS),
             lists:foldl(fun(#metric_data{value = V}, Acc) -> Acc + V end, 0, Selection);
         false ->
--- src/ditax_metrics.erl
+++ src/ditax_metrics.erl
@@ -19,12 +19,14 @@
 ]).
 
 %% gen_server callbacks
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
 
 -define(SERVER, ?MODULE).
 
@@ -36,12 +38,6 @@
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
 start_link(MetricsList) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [MetricsList], []).
 
@@ -70,167 +66,125 @@ get(MetricName, Period) ->
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([MetricsList]) ->
-    
-    io:format("--------INIT ~p~n", [?MODULE]),
-    
-    State = lists:foldl(fun(M, S) ->
-        case lists:member(M, S#state.metrics) of
-            true -> S;
-            false -> create_metric(M, S)
-        end
-    end, #state{}, MetricsList),
+    State = lists:foldl(
+        fun(M, S) ->
+            case lists:member(M, S#state.metrics) of
+                true -> S;
+                false -> create_metric(M, S)
+            end
+        end,
+        #state{},
+        MetricsList
+    ),
     {ok, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-                  State :: #state{}) ->
-                     {reply, Reply :: term(), NewState :: #state{}} |
-                     {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-                     {noreply, NewState :: #state{}} |
-                     {noreply, NewState :: #state{}, timeout() | hibernate} |
-                     {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-                     {stop, Reason :: term(), NewState :: #state{}}).
+-spec handle_call(
+    Request :: term(),
+    From :: {pid(), Tag :: term()},
+    State :: #state{}
+) ->
+    {reply, Reply :: term(), NewState :: #state{}}.
 handle_call({get, MetricName}, _From, State) ->
-    Res = case lists:member(MetricName, State#state.metrics) of
-        true ->
-            gen_server:call(MetricName, get);
-        _ ->
-            lager:error("~p table does not exist ~p", [MetricName, State#state.metrics]),
-            {error, wrong_metric}
-    end,
+    Res =
+        case lists:member(MetricName, State#state.metrics) of
+            true ->
+                gen_server:call(MetricName, get);
+            _ ->
+                logger:error("~p table does not exist ~p", [MetricName, State#state.metrics]),
+                {error, wrong_metric}
+        end,
     {reply, Res, State};
 handle_call({get, MetricName, Period}, _From, State) ->
-    Res = case lists:member(MetricName, State#state.metrics) of
-              true ->
-                  gen_server:call(MetricName, {get, Period});
-              _ ->
-                  lager:error("~p table does not exist", [MetricName]),
-                  {error, wrong_metric}
-          end,
+    Res =
+        case lists:member(MetricName, State#state.metrics) of
+            true ->
+                gen_server:call(MetricName, {get, Period});
+            _ ->
+                logger:error("~p table does not exist", [MetricName]),
+                {error, wrong_metric}
+        end,
     {reply, Res, State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_cast(Request :: term(), State :: #state{}) ->
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), NewState :: #state{}}).
+-spec handle_cast(Request :: term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}}.
 handle_cast({add, #metric{name = MetricName} = Metric}, State) ->
-    State1 = case lists:member(MetricName, State#state.metrics) of
-                 false ->
-                     create_metric(Metric, State);
-                 _ ->
-                     lager:error("~p table already exists", [MetricName]),
-                     State
-             end,
+    State1 =
+        case lists:member(MetricName, State#state.metrics) of
+            false ->
+                create_metric(Metric, State);
+            _ ->
+                logger:error("~p table already exists", [MetricName]),
+                State
+        end,
     {noreply, State1};
 handle_cast({add, MetricName}, State) ->
-    State1 = case lists:member(MetricName, State#state.metrics) of
-                false ->
-                    create_metric(MetricName, State);
-                _ ->
-                    lager:error("~p table already exists", [MetricName]),
-                    State
-            end,
+    State1 =
+        case lists:member(MetricName, State#state.metrics) of
+            false ->
+                create_metric(MetricName, State);
+            _ ->
+                logger:error("~p table already exists", [MetricName]),
+                State
+        end,
     {noreply, State1};
 handle_cast({MetricName, increment, Value}, State) ->
+    % logger:debug("--------2-------INC ~p", [MetricName]),
     case lists:member(MetricName, State#state.metrics) of
         true ->
+            % logger:debug("--------3-------INC ~p", [MetricName]),
             gen_server:cast(MetricName, {increment, Value});
         _ ->
-            lager:error("~p table does not exist", [MetricName])
+            % logger:debug("--------4-------INC ~p", [MetricName]),
+            logger:error("~p table does not exist", [MetricName])
     end,
     {noreply, State};
 handle_cast(delete, State) ->
-    State1 = lists:foldl(fun(M, S) ->
-        ok = gen_server:cast(M, {delete, M}),
-        CurrentMetrics = S#state.metrics,
-        S#state{metrics = lists:delete(M, CurrentMetrics)}
-    end, State, State#state.metrics),
+    State1 = lists:foldl(
+        fun(M, S) ->
+            ok = gen_server:cast(M, {delete, M}),
+            CurrentMetrics = S#state.metrics,
+            S#state{metrics = lists:delete(M, CurrentMetrics)}
+        end,
+        State,
+        State#state.metrics
+    ),
     {noreply, State1};
 handle_cast({delete, MetricName}, State) ->
-    State1 = case lists:member(MetricName, State#state.metrics) of
-        true ->
-            ok = gen_server:cast(MetricName, {delete, MetricName}),
-            CurrentMetrics = State#state.metrics,
-            State#state{metrics = lists:keydelete(MetricName, 1, CurrentMetrics)};
-        _ ->
-            lager:error("~p table does not exist", [MetricName]),
-            State
-    end,
+    State1 =
+        case lists:member(MetricName, State#state.metrics) of
+            true ->
+                ok = gen_server:cast(MetricName, {delete, MetricName}),
+                CurrentMetrics = State#state.metrics,
+                State#state{metrics = lists:keydelete(MetricName, 1, CurrentMetrics)};
+            _ ->
+                logger:error("~p table does not exist", [MetricName]),
+                State
+        end,
     {noreply, State1};
 handle_cast(_Request, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-    {noreply, NewState :: #state{}} |
-    {noreply, NewState :: #state{}, timeout() | hibernate} |
-    {stop, Reason :: term(), NewState :: #state{}}).
+-spec handle_info(Info :: timeout() | term(), State :: #state{}) ->
+    {noreply, NewState :: #state{}}.
 handle_info(_Info, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-                State :: #state{}) -> term()).
+-spec terminate(
+    Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #state{}
+) -> term().
 terminate(_Reason, _State) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-                  Extra :: term()) ->
-                     {ok, NewState :: #state{}} | {error, Reason :: term()}).
+-spec code_change(
+    OldVsn :: term() | {down, term()},
+    State :: #state{},
+    Extra :: term()
+) ->
+    {ok, NewState :: #state{}}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
